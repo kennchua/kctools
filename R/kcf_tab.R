afc56ceb12d2749e40dca118747a1726405159e3
Kenn Chua <chuax025@umn.edu>
2022-07-29 02:42:09 -0700

    Update to tidytable v0.8.0

--- R/kcf_tab.R
+++ R/kcf_tab.R
@@ -16,6 +16,5 @@ kcf_tab <- function(data, tabvar, dropna = FALSE) {
   data |>
     {\(df) if (dropna == TRUE) tidytable::filter.(df, tidytable::across.(c({{ tabvar }}), ~ !is.na(.))) else df}() |>
     tidytable::count.({{ tabvar }}) |>
-    tidytable::rename.(n = N) |>
     tidytable::mutate.(prop = n/sum(n))
 }
