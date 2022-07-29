7c23afa318e89b3b52fae03dcb7b0c0023a03068
Kenn Chua <chuax025@umn.edu>
2022-07-29 03:50:22 -0700

    Update R/kcf_tab.R

--- R/kcf_tab.R
+++ R/kcf_tab.R
@@ -5,6 +5,7 @@
 #' @param tabvar The variables to tabulate.
 #' @return A data frame with tabulation of counts and proportions.
 #' @import tidytable
+#' @importFrom data.table :=
 #' @examples
 #' \dontrun{
 #' kcf_tab(mtcars, c(vs)) # one-way tabulate
@@ -19,4 +20,3 @@ kcf_tab <- function(data, tabvar, dropna = FALSE) {
     tidytable::mutate.(prop = n/sum(n))
 }
 
-.datatable.aware <- TRUE
