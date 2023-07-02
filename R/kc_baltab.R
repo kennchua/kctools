6653c1438539ed098a92c1e66b33585883861c14
Kenn Chua <44691209+kennchua@users.noreply.github.com>
2023-07-02 03:31:42 -0700

    Update kc_baltab

--- R/kc_baltab.R
+++ R/kc_baltab.R
@@ -1,6 +1,6 @@
-#' Create balance table with group means and standard deviations or standard error; conduct t-test and F-test
+#' Create balance table with group statistics; conduct t-tests and F-test
 #'
-#' Compute group means, standard deviations, as well as standard errors and conducts difference-in-means test with respect to reference group
+#' Computes group means, standard deviations, as well as standard errors; conducts difference-in-means test with respect to reference group; conduct joint significance test
 #' @param data The data frame to tabulate
 #' @param balvar The variables to tabulate
 #' @param grpvar If TRUE then drops NA values; if FALSE then includes NA values
@@ -9,7 +9,7 @@
 #' @param vcov Standard error type (e.g. "hc1", "iid", "hetero") or a name of a variable to cluster standard errors on
 #' @param report_sd If TRUE then reports standard deviation of each variable; if FALSE then reports standard errors
 #' @param report_ttest_pval If TRUE then reports p-values in difference in means test; if FALSE then reports mean difference
-#' @return A data frame with tabulation of counts and proportions.
+#' @return A data frame with group statistics as well as output from difference-in-means tests and F-test.
 #' @import dplyr
 #' @import tidyr
 #' @import stringr
